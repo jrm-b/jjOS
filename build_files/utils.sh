b0751d301dc8400f67b5fba72f6ddeaa337ea6d7
jeremy <135468931+jrm-b@users.noreply.github.com.>
2026-05-20 22:04:33 +0200

    feat: add copr_install_isolated helper (ublue)

--- build_files/utils.sh
+++ build_files/utils.sh
@@ -14,6 +14,37 @@ log() {
     printf '[%s] '"${level_color}"'%s\033[0m %s\n' "$(date '+%Y-%m-%d %H:%M:%S')" "$level" "$*"
 }
 
+copr_install_isolated() {
+    if [[ $# -lt 1 || -z "$1" ]]; then
+        log ERROR "Missing COPR repository target for copr_install_isolated"
+        exit 1
+    fi
+
+    local copr_name="$1"
+    shift
+    local packages=("$@")
+
+    if [[ ${#packages[@]} -eq 0 ]]; then
+        log ERROR "No packages specified for copr_install_isolated"
+        return 1
+    fi
+
+    repo_id="copr:copr.fedorainfracloud.org:${copr_name//\//:}"
+    
+    log INFO "Installing ${packages[*]} from COPR $copr_name (isolated)"
+
+    if ! command -v dnf5 &>/dev/null; then
+        log ERROR "Missing dnf5 dependency for copr_install_isolated"
+        return 1
+    fi 
+
+    dnf5 -y copr enable "$copr_name"
+    dnf5 -y copr disable "$copr_name"
+    dnf5 -y install --enablerepo="$repo_id" "${packages[@]}"
+
+    log INFO "Installed ${packages[*]} from $copr_name"
+}
+
 trap '
     if [[ $BASH_COMMAND != echo* && $BASH_COMMAND != log* ]]; then
         log DEBUG "$BASH_COMMAND"
